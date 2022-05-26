7e4676b93305a3ceaa716d9090e5c97ac117f1f1
lorececco <73065168+lorececco@users.noreply.github.com>
2022-05-26 15:18:04 +0200

    g
    g

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 public class DAO {
 
     private String message;
-    private static final String url1 = "jdbc:mysql://localhost:3306/dbprog";
+    private static final String url1 = "jdbc:mysql://127.0.0.1/dbprog";
     private static final String user = "root";
-    private static final String password = "tweb";
+    private static final String password = "";
     private static Connection conn1 = null;
 
     public static Connection getConn1() {
